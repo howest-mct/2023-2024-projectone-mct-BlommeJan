9066fddfc52b1fc84eeeae388818059eb3987ee3
BlommeJan <jan.blomme@student.howest.be>
2024-06-15 21:36:06 +0200

    chore: Refactor motor speed control code for smoother transitions and improved performance

--- backend/helpers/trilMotorPwm.py
+++ backend/helpers/trilMotorPwm.py
@@ -21,36 +21,36 @@ class DCMotor:
         GPIO.cleanup()
 
 # example usage
-# if __name__ == "__main__":
-#     try:
-#         motor = DCMotor(12)
-#         while True:
-#             print("Turning on motor")
-#             for i in range(0, 101, 5):
-#                 motor.changeSpeed(i)
-#                 sleep(0.1)
-#             print("Motor at its max speed")
-#             sleep(10)
-#             print("Turning off motor")
-#             for i in range(100, -1, -5):
-#                 motor.changeSpeed(i)
-#                 sleep(0.1)
-#             print("Motor stopped")
-#             sleep(5)
-#     except KeyboardInterrupt as e:
-#         motor.cleanup()
-#         print("\nQuitting...")
-
 if __name__ == "__main__":
     try:
         motor = DCMotor(25)
         while True:
             print("Turning on motor")
-            motor.changeSpeed(100)
+            for i in range(0, 101, 5):
+                motor.changeSpeed(i)
+                sleep(0.2)
+            print("Motor at its max speed")
             sleep(10)
             print("Turning off motor")
-            motor.stop()
+            for i in range(100, -1, -5):
+                motor.changeSpeed(i)
+                sleep(0.1)
+            print("Motor stopped")
             sleep(5)
     except KeyboardInterrupt as e:
         motor.cleanup()
-        print("\nQuitting...")
+#         print("\nQuitting...")
+
+# if __name__ == "__main__":
+    # try:
+    #     motor = DCMotor(25)
+    #     while True:
+    #         print("Turning on motor")
+    #         motor.changeSpeed(100)
+    #         sleep(10)
+    #         print("Turning off motor")
+    #         motor.stop()
+    #         sleep(5)
+    # except KeyboardInterrupt as e:
+    #     motor.cleanup()
+    #     print("\nQuitting...")
