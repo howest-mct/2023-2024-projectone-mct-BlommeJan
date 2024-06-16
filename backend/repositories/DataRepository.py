fc23f72ad8fcd632593b4da093c3af9665002f57
BlommeJan <jan.blomme@student.howest.be>
2024-06-16 16:38:36 +0200

    chore: Remove unused code and configuration files

--- backend/repositories/DataRepository.py
+++ backend/repositories/DataRepository.py
@@ -9,32 +9,3 @@ class DataRepository:
         else:
             gegevens = request.form.to_dict()
         return gegevens
-
-    @staticmethod
-    def read_status_lampen():
-        sql = "SELECT * from lampen"
-        return Database.get_rows(sql)
-
-    @staticmethod
-    def read_status_lamp_by_id(id):
-        sql = "SELECT * from lampen WHERE id = %s"
-        params = [id]
-        return Database.get_one_row(sql, params)
-
-    @staticmethod
-    def update_status_lamp(id, status):
-        sql = "UPDATE lampen SET status = %s WHERE id = %s"
-        params = [status, id]
-        return Database.execute_sql(sql, params)
-
-    @staticmethod
-    def update_status_alle_lampen(status):
-        sql = "UPDATE lampen SET status = %s"
-        params = [status]
-        return Database.execute_sql(sql, params)
-
-    @staticmethod
-    def insert_temperature(temp):
-        sql = "INSERT INTO logtemp (dateTime, tempInC) VALUES (NOW(), %s)"
-        params = [temp]
-        return Database.execute_sql(sql, params)
